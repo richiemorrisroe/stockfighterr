1eba8e5f8c35c315a0ab0dfdcd435410393f23c7
richie <richie.morrisroe@gmail.com>
2016-02-16 12:41:37 +0000

    lint changes

--- tests/testthat/test_api.R
+++ tests/testthat/test_api.R
@@ -1,12 +1,7 @@
 orderb <- get_orderbook("TESTEX", "FOOBAR")
 test_that("test venue is available", {
-    expect_equal(httr::status_code(get_orderbook("TESTEX", "FOOBAR")), 200)})
-
-
-
-
-
-
+    expect_equal(httr::status_code(get_orderbook("TESTEX", "FOOBAR")), 200)
+})
 test_that("get_quote returns a response",
           expect_is(get_quote("TESTEX", "FOOBAR"), "response"))
 
@@ -17,9 +12,3 @@ test_that("get_orderbook returns a response", {
 test_that("get_tickers returns a response", {
     expect_is(get_tickers("TESTEX"), "response")
 })
-
-
-
-
-
-
