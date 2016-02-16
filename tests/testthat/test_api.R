5674bb48c857f5491bc2219e975a544577078913
richie <richie.morrisroe@gmail.com>
2016-02-16 12:34:54 +0000

    fixed tests with skip_on_cran

--- tests/testthat/test_api.R
+++ tests/testthat/test_api.R
@@ -1,16 +1,7 @@
 orderb <- get_orderbook("TESTEX", "FOOBAR")
 test_that("test venue is available", {
     expect_equal(httr::status_code(get_orderbook("TESTEX", "FOOBAR")), 200)})
-ordbuy <- create_order(account = "EXB123456",
-                                         venue = "TESTEX",
-                                         stock = "FOOBAR",
-                                         price = 2000,
-                                         qty = 200,
-                                         direction = "buy",
-                       ordertype = "limit")
 
-test_that("create order returns a list",
-          expect_is(ordbuy, "list" ))
 
 
 
@@ -27,7 +18,6 @@ test_that("get_tickers returns a response", {
     expect_is(get_tickers("TESTEX"), "response")
 })
 
-placebuy.p <- parse_response(placebuy)
 
 
 
--- tests/testthat/test_ord.R
+++ tests/testthat/test_ord.R
@@ -1,20 +1,43 @@
-skip_on_cran("This test requires an API key")
+ordbuy <- create_order(account = "EXB123456",
+                                         venue = "TESTEX",
+                                         stock = "FOOBAR",
+                                         price = 2000,
+                                         qty = 200,
+                                         direction = "buy",
+                       ordertype = "limit")
+
+test_that("create order returns a list",
+          expect_is(ordbuy, "list" ))
+test_that("place_order returns a response", {
+    skip_on_cran("This test requires an API key")
+placebuy <- place_order("TESTEX", "FOOBAR",
+                        body = ordbuy, apikey=apikey)
+    expect_is(placebuy, "response")})
+
+
 test_that("get_all_orders returns a response", {
+    skip_on_cran("This test requires an API key")
     expect_is(get_all_orders("TESTEX", "EXB123456", apikey), "response")})
-skip_on_cran("This test requires an API key")
+
+
 test_that("get_order_status returns a response", {
+    skip_on_cran("This test requires an API key")
           expect_is(
               get_order_status(
                   placebuy.p$id,
                   "TESTEX",
                   "FOOBAR"), "response")})
-skip_on_cran("This test requires an API key")
+
+
 test_that("cancel order returns a response", {
+    skip_on_cran("This test requires an API key")
     expect_is(cancel_order(placebuy.p$id,
                            "TESTEX",
                            "FOOBAR"), "response")})
-skip_on_cran("This test requires an API key")
+
+
 test_that("ioc orders are always closed after response", {
+    skip_on_cran("This test requires an API key")
     ord <- create_order("EXB123456",
                         "TESTEX",
                         "FOOBAR",
@@ -25,25 +48,30 @@ test_that("ioc orders are always closed after response", {
     placed <- place_order("TESTEX", "FOOBAR", body=ord, apikey=apikey)
     placedp <- parse_response(placed)
     expect_equal(placedp$open, FALSE) })
-skip_on_cran("This test requires an API key")
-placebuy <- place_order("TESTEX", "FOOBAR",
-                        body = ordbuy, apikey=apikey)
-skip_on_cran("This test requires an API key")
-test_that("parse_response returns a list from an order object",
-          expect_is(parse_response(placebuy), "list"))
-skip_on_cran("This test requires an API key")
-test_that("a placed order returns a totalFilled numeric greater or equal to 0", {expect_gte(placebuyp$totalFilled, 0)
-})
-skip_on_cran("This test requires an API key")
+
+
+test_that("parse_response returns a list from an order object", {
+    skip_on_cran("This test requires an API key")
+          expect_is(parse_response(placebuy), "list")})
+skip_on_cran("This test requires an API key") 
+test_that(
+    skip_on_cran("This test requires an API key")
+    "a placed order returns a totalFilled numeric greater than 0", {
+        expect_gte(placebuyp$totalFilled, 0)
+    })
+
 test_that("place_order returns a response", {
+    skip_on_cran("This test requires an API key")
     expect_is(placebuy, "response")
 })
-skip_on_cran("This test requires an API key")
+
 apikey <- get_api_key("~/Dropbox/Code/Starfighter/apikey.txt")
 test_that("get_api_key returns a character vector", {
+    skip_on_cran("This test requires an API key")
     expect_is(apikey, "character")
 })
 skip_on_cran("This test requires an API key")
 test_that("api key is length one", {
+    skip_on_cran("This test requires an API key")
     expect_equal(length(apikey), 1)
 })
